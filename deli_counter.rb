cd58e1a023f0afabfd8d2fb533676ee6407ecf13
Adam Ye <adamye48@gmail.com>
2020-08-13 14:07:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def line(katz_deli)
     puts "The line is currently empty."
       else
       message="The line is currently:"
-      other_deli.each_with_index do |value, index|
+      katz.each_with_index do |value, index|
       message += " #{index.to_i+1}. #{value}"
     end
     puts "#{message}"
