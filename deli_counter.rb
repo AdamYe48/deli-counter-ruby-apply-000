48d6349375959d49f60bff597e8cbc16e08a349f
Adam Ye <adamye48@gmail.com>
2020-08-13 14:27:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,19 @@
 # Write your code here.
 #katz_deli = []
 
-# def take_a_number(katz_deli, name)
-#   katz_deli << name
-#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-# end
-
-number = 0 
-def take_a_number(katz_deli)
-  
-  number += 1
-  katz_deli << number
-  puts "welcome, you are number #{katz_deli.length} in line."
-  
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+# number = 0 
+# def take_a_number(katz_deli)
+  
+#   number += 1
+#   katz_deli << number
+#   puts "welcome, you are number #{katz_deli.length} in line."
+  
+# end
   
   
 def now_serving(katz_deli)
@@ -30,7 +30,7 @@ def line(katz_deli)
     puts "The line is currently empty."
       else
       message="The line is currently:"
-      katz.each_with_index do |value, index|
+      katz_deli.each_with_index do |value, index|
       message += " #{index.to_i+1}. #{value}"
     end
     puts "#{message}"
